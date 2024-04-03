27f802083571bc3b1a76c7b0bdc33966ef932b41
Jatin Thakur <73630627+JatinThakur-12@users.noreply.github.com>
2024-04-03 14:14:52 +0530

    done initial setup with richtexteditor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="stylesheet" href="/richtexteditor/rte_theme_default.css" />  
+    <script type="text/javascript" src="/richtexteditor/rte.js"></script>  
+    <script type="text/javascript" src='/richtexteditor/plugins/all_plugins.js'></script> 
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
